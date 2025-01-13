04e31a7906b0978328424d988c62a8b161f4c579
vikash032002 <vikuvikash03@gmail.com>
2025-01-13 20:55:35 +0530

    Lec1

---
+++ namste-react/Lec1/App.js
@@ -0,0 +1,22 @@
+const react1=React.createElement("h1",{id:"heading"},"Hello world from React");
+console.log(react1);//object
+ 
+
+{/* <div id="parents">
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div>   create this with react*/}
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+     React.createElement("div",
+        {id:"child"},
+        [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
+     ));
+const root1=ReactDOM.createRoot(document.getElementById("root"));
+root1.render(parent);
+
+console.log(parent);
