173b1e2bc1f71db588f73fe45c34617fe2f37b8f
vikash032002 <vikuvikash03@gmail.com>
2025-01-16 22:50:04 +0530

    Lec3

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const react1=React.createElement("h1",{id:"heading"},"Hello world from React");
-console.log(react1);//object
- 
 
-{/* <div id="parents">
-    <div id="child">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div>   create this with react*/}
+// React element
+const heading = <h1>Namste React by JSX</h1>;
 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-     React.createElement("div",
-        {id:"child"},
-        [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
-     ));
-const root1=ReactDOM.createRoot(document.getElementById("root"));
-root1.render(parent);
+// React Functional component(just normal js function)
+const Title = () =>(
+     <h1>Hello</h1>
+);
 
-console.log(parent);
+const HeadingComponent = () =>(
+     <div>
+        <Title />
+        {heading}
+        <h1>Namste React by Functional component</h1>
+</div> );
+     
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render([heading,<HeadingComponent />]);
+
